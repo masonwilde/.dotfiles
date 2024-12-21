a96a8b5240b35418d6805e546f5b020554f368aa
Mason Wilde <mason@wilde.dev>
2024-12-21 13:26:46 -0700

    Major refactor of neovim configs

---
+++ nvim/.config/nvim/lua/masonwilde/plugins/lualine.lua
@@ -0,0 +1,9 @@
+-- nvim/lua/masonwilde/plugins/lualine.lua
+
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup()
+    end,
+}
