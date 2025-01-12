987bacc527249b7a269860a4eede5e9815d49a19
Mason Wilde <mason@wilde.dev>
2025-01-11 22:33:23 -0700

    Add fzflua to nvim

---
+++ nvim/.config/nvim/lua/masonwilde/plugins/fzf.lua
@@ -0,0 +1,10 @@
+-- nvim/lua/masonwilde/plugins/fzf.lua
+
+return {
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- or if using mini.icons/mini.nvim
+	-- dependencies = { "echasnovski/mini.icons" },
+	opts = {},
+}
