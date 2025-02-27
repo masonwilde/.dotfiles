a80ad8cabdd9df53a93fcdf1aaedf4e9c92665fa
Mason Wilde <mason@wilde.dev>
2025-02-27 09:15:08 -0700

    Add codecompanion using anthropic

---
+++ nvim/.config/nvim/lua/masonwilde/plugins/codecompanion.lua
@@ -0,0 +1,50 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		local dotenv = require("lua-dotenv")
+		dotenv.load_dotenv()
+		require("codecompanion").setup({
+			adapters = {
+				anthropic = function()
+					return require("codecompanion.adapters").extend("anthropic", {
+						env = {
+							api_key = dotenv.get("ANTHROPIC_API_KEY"),
+						},
+					})
+				end,
+			},
+			strategies = {
+				chat = { adapter = "anthropic" },
+				inline = { adapter = "anthropic" },
+				agent = { adapter = "anthropic" },
+			},
+			display = {
+				action_palette = {
+					width = 95,
+					height = 10,
+					prompt = "Prompt ", -- Prompt used for interactive LLM calls
+					provider = "telescope", -- default|telescope|mini_pick
+					opts = {
+						show_default_actions = true, -- Show the default actions in the action palette?
+						show_default_prompt_library = true, -- Show the default prompt library in the action palette?
+					},
+				},
+			},
+		})
+		vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+		vim.keymap.set(
+			{ "n", "v" },
+			"<Leader>a",
+			"<cmd>CodeCompanionChat Toggle<cr>",
+			{ noremap = true, silent = true }
+		)
+		vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+		-- Expand 'cc' into 'CodeCompanion' in the command line
+		vim.cmd([[cab cc CodeCompanion]])
+	end,
+}
