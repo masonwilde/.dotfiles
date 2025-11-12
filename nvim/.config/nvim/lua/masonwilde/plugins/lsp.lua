562841bfd038b25da3de3a5c924ef7a02e36b553
Mason Wilde <mason@wilde.dev>
2025-11-12 06:28:05 -0700

    Simplify LSP

--- nvim/.config/nvim/lua/masonwilde/plugins/lsp.lua
+++ nvim/.config/nvim/lua/masonwilde/plugins/lsp.lua
@@ -1,164 +1,68 @@
 -- nvim/lua/masonwilde/plugins/lsp.lua
 
 return {
-	{
-		"mason-org/mason.nvim",
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-			"WhoIsSethDaniel/mason-tool-installer.nvim",
+	"mason-org/mason-lspconfig.nvim",
+	opts = {
+		ensure_installed = {
+			"clangd",
+			"cssls",
+			"eslint",
+			"html",
+			"jsonls",
+			"lua_ls",
+			"pyright",
+			"tailwindcss",
+			"ruff",
 		},
-		config = function()
-			require("mason").setup()
-
-			require("mason-lspconfig").setup({
-				automatic_installation = true,
-				ensure_installed = {
-					"clangd",
-					"cssls",
-					"eslint",
-					"html",
-					"jsonls",
-					"lua_ls",
-					"pyright",
-					"tailwindcss",
-					"ruff",
-				},
-			})
-
-			require("mason-tool-installer").setup({
-				ensure_installed = {
-					"prettier",
-					"stylua", -- lua formatter
-					"pylint",
-					"eslint_d",
-				},
-			})
-		end,
+		automatic_enable = {
+			exclude = { "lua_ls", "cssls", "ruff" }, -- Exclude servers you want to configure manually
+		},
 	},
-	{
+	dependencies = {
+		"mason-org/mason.nvim",
 		"neovim/nvim-lspconfig",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			{ "folke/neodev.nvim", opts = {} },
-		},
-		config = function()
-			local nvim_lspconfig = require("lspconfig")
-			local mason_lspconfig = require("mason-lspconfig")
+	},
+	config = function()
+		-- Diagnostic keymaps
+		vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+		vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
-			local on_attach = function(client, bufnr)
-				if client.server_capabilities.documentFormattingProvider then
+		-- LSP keymaps and auto-format
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+				local client = vim.lsp.get_client_by_id(ev.data.client_id)
+				if client and client.server_capabilities.documentFormattingProvider then
 					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = vim.api.nvim_create_augroup("Format", { clear = true }),
-						buffer = bufnr,
+						group = vim.api.nvim_create_augroup("Format_" .. ev.buf, { clear = true }),
+						buffer = ev.buf,
 						callback = function()
-							vim.lsp.buf.format()
+							vim.lsp.buf.format({ async = false })
 						end,
 					})
 				end
-			end
 
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-			mason_lspconfig.setup_handlers({
-				function(server)
-					nvim_lspconfig[server].setup({
-						capabilities = capabilities,
-					})
-				end,
-				["lua_ls"] = function()
-					require("lspconfig").lua_ls.setup({
-						on_attach = on_attach,
-						capabilities = capabilities,
-						settings = {
-							Lua = {
-								diagnostics = {
-									globals = { "vim" },
-								},
-								workspace = {
-									library = vim.api.nvim_get_runtime_file("", true),
-								},
-							},
-						},
-					})
-				end,
-				["ruff"] = function()
-					require("lspconfig").ruff.setup({
-						on_attach = on_attach,
-						capabilities = capabilities,
-					})
-				end,
-				["cssls"] = function()
-					nvim_lspconfig["cssls"].setup({
-						on_attach = on_attach,
-						capabilities = capabilities,
-					})
-				end,
-				["tailwindcss"] = function()
-					nvim_lspconfig["tailwindcss"].setup({
-						on_attach = on_attach,
-						capabilities = capabilities,
-					})
-				end,
-				["html"] = function()
-					nvim_lspconfig["html"].setup({
-						on_attach = on_attach,
-						capabilities = capabilities,
-					})
-				end,
-				["jsonls"] = function()
-					nvim_lspconfig["jsonls"].setup({
-						on_attach = on_attach,
-						capabilities = capabilities,
-					})
-				end,
-				["eslint"] = function()
-					nvim_lspconfig["eslint"].setup({
-						on_attach = on_attach,
-						capabilities = capabilities,
-					})
-				end,
-				["pyright"] = function()
-					nvim_lspconfig["pyright"].setup({
-						on_attach = on_attach,
-						capabilities = capabilities,
-					})
-				end,
-			})
-
-			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
-			-- Use LspAttach autocommand to only map the following keys
-			-- after the language server attaches to the current buffer
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					-- Enable completion triggered by <c-x><c-o>
-					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-					-- Buffer local mappings.
-					-- See `:help vim.lsp.*` for documentation on any of the below functions
-					local opts = { buffer = ev.buf }
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-					vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-					vim.keymap.set("n", "<space>wl", function()
-						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-					end, opts)
-					vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-					vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-					vim.keymap.set("n", "<space>f", function()
-						vim.lsp.buf.format({ async = true })
-					end, opts)
-				end,
-			})
-		end,
-	},
+				local opts = { buffer = ev.buf }
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+				vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+				vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+				vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+				vim.keymap.set("n", "<space>wl", function()
+					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+				end, opts)
+				vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+				vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+				vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				vim.keymap.set("n", "<space>f", function()
+					vim.lsp.buf.format({ async = true })
+				end, opts)
+			end,
+		})
+	end,
 }
